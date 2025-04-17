baa213c589fc060ff0bd4f1d5b58baadd43de445
asoiso <asoiso@foxmail.com>
2025-04-17 17:56:38 +0800

    feat(*): init

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+// https://prettier.io/docs/en/options.html
+
+module.exports = {
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 110,
+  endOfLine: 'lf',
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+};
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # jdsc-scaffold
+
+https://nextjs.org/
+
+https://strapi.io/
+
+https://pro.ant.design/zh-CN/
+
+https://nuxt.com/
+
+https://www.netlify.com/
+
+https://www.vercel.com/
+
+https://www.cloudflare.com/
+
+
